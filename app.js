1c755f836429fe566df3e34d8700c41515c40af5
sisandashange25-eng <sisandashange25@gmail.com>
2026-09-22 11:52:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ async function loadProducts() {
         price: Number(p.price),
         cat: p.category || "Other",
         icon: "🛍️",
-        seller: p.seller || "Zava Seller",
+        seller:"Zava Seller",
         image_url: p.image_url || ""
       }));
 
